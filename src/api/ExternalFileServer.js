197fa6ded59bf3158c756a12fdd4051420fe8b45
Medrano Matias Exequiel <mmedrano@beesion.com>
2022-12-08 17:34:15 -0300

    Add Test for Real API Rest

--- src/api/ExternalFileServer.js
+++ src/api/ExternalFileServer.js
@@ -1,24 +1,61 @@
-const http = require('http')
-const util = require('util')
+const https = require('https')
 
-const httpGetRequest = util.promisify(http.get)
+async function HttpGetRequest(url, headers = {}, callback) {
+    return new Promise((resolve, reject) => {
+        const req = 
+        https.request(
+            url, 
+            { headers },
+            res => {
+                if (res.statusCode < 200 || res.statusCode >= 300) {
+                  return resolve(``);
+                }
+          
+                const data = [];
+          
+                res.on('data', chunk => {
+                  data.push(chunk);
+                });
+          
+                res.on('end', () => {
+                    resolve(callback(Buffer.concat(data).toString()))
+                });
+            }
+        );
+        req.on('error', reject);
+        req.end();
+    })    
+}
 
 class ExternalFileServer {
     constructor() {
 
     }
     async listFiles() {
-        let res = await httpGetRequest("https://echo-serv.tbxnet.com/v1/secret/files", {
-            headers : {
-                authorization: "Bearer aSuperSecretKey"
+        return HttpGetRequest(
+            "https://echo-serv.tbxnet.com/v1/secret/files",
+            {
+                authorization: "Bearer aSuperSecretKey",
+                Accept: "application/json"
+            },
+            function(response) {
+                let objResponse = JSON.parse(response)
+                return objResponse.files
             }
-        })
-        console.log(res)
-        return res
+        )
     }
+
     async retrieveContent(fileName) {
-        return `file,text,number,hex
-        file1.csv,RgTya,64075909`
+        return HttpGetRequest(
+            `https://echo-serv.tbxnet.com/v1/secret/file/${fileName}`,
+            {
+                authorization: "Bearer aSuperSecretKey",
+                Accept: "application/json"
+            },
+            function(response) {
+                return response
+            }
+        )
     }
 }
 
